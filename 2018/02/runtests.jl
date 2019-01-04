2b04349fffa2944a2157fae3ff7e05981dca20d9
Stephen Feagin <stephenfeagin@gmail.com>
2019-01-03 19:00:35 -0500

    Reformat 2018.2 tests

--- 2018/02/runtests.jl
+++ 2018/02/runtests.jl
@@ -2,26 +2,24 @@ using Test
 
 include("day_02.jl")
 
-@testset "Test Part 1" begin
-    @test part_1([
-        "abcdef",
-        "bababc",
-        "abbcde",
-        "abcccd",
-        "aabcdd",
-        "abcdee",
-        "ababab"
-    ]) == 12 
-end
+# "Test Part 1"
+@test part_1([
+    "abcdef",
+    "bababc",
+    "abbcde",
+    "abcccd",
+    "aabcdd",
+    "abcdee",
+    "ababab"
+]) == 12 
 
-@testset "Test Part 2" begin
-    @test part_2([
-        "abcde",
-        "fghij",
-        "klmno",
-        "pqrst",
-        "fguij",
-        "axcye",
-        "wvxyz"
-    ]) == "fgij"
-end
+# "Test Part 2" 
+@test part_2([
+    "abcde",
+    "fghij",
+    "klmno",
+    "pqrst",
+    "fguij",
+    "axcye",
+    "wvxyz"
+]) == "fgij"
